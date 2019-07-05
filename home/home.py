9e1dac377b479d8ea9fc9181e17a81efb85d8e1f
Manh Tu <glmanhtu@gmail.com>
2019-07-05 07:53:26 +0100

    Remove TrainNewModel UI, since mostly people will prefer to train though terminal, on a remote machine rather than current machine

--- home/home.py
+++ home/home.py
@@ -1,10 +1,12 @@
 from PyQt5.QtCore import QRect, QMetaObject, QCoreApplication
-from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QTabWidget, QMenuBar
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QMenuBar
 
 from common.services import data_transporter_service
 from common.utils.constants import WINDOW_CHANEL
 from home.load_trained_model import LoadTrainedModelWidget
-from home.train_new_model import TrainNewModel
+
+
+# from home.train_new_model import TrainNewModel
 
 
 class Home(QMainWindow):
@@ -12,7 +14,7 @@ class Home(QMainWindow):
         super(Home, self).__init__()
         self.setupUi(self)
         self.show()
-        self.tabs.setCurrentIndex(0)
+        # self.tabs.setCurrentIndex(0)
 
     def setupUi(self, window):
         window.resize(600, 236)
@@ -21,22 +23,21 @@ class Home(QMainWindow):
         vertical_layout = QVBoxLayout(central_widget)
         vertical_layout.setContentsMargins(11, 11, 11, 11)
         vertical_layout.setSpacing(6)
-        self.tabs = QTabWidget(central_widget)
-        self.tabs.setTabShape(QTabWidget.Rounded)
-        self.first_tab = LoadTrainedModelWidget()
-        self.tabs.addTab(self.first_tab, "")
-
-        self.second_tab = TrainNewModel()
-        self.tabs.addTab(self.second_tab, "")
-        vertical_layout.addWidget(self.tabs)
-        window.setCentralWidget(central_widget)
+        # self.tabs = QTabWidget(central_widget)
+        # self.tabs.setTabShape(QTabWidget.Rounded)
+        # self.first_tab = LoadTrainedModelWidget()
+        # self.tabs.addTab(self.first_tab, "")
+        #
+        # self.second_tab = TrainNewModel()
+        # self.tabs.addTab(self.second_tab, "")
+        # vertical_layout.addWidget(self.tabs)
+        window.setCentralWidget(LoadTrainedModelWidget())
         self.menuBar = QMenuBar(window)
         self.menuBar.setGeometry(QRect(0, 0, 600, 22))
         self.menuBar.setObjectName("menuBar")
         window.setMenuBar(self.menuBar)
 
         self.retranslateUi(window)
-        self.tabs.setCurrentIndex(1)
         QMetaObject.connectSlotsByName(window)
 
         data_transporter_service.listen(WINDOW_CHANEL, self.openWindow)
@@ -44,8 +45,8 @@ class Home(QMainWindow):
     def retranslateUi(self, window):
         _translate = QCoreApplication.translate
         window.setWindowTitle(_translate("MainWindow", "Face Expression"))
-        self.tabs.setTabText(self.tabs.indexOf(self.first_tab), _translate("MainWindow", "Load a trained model"))
-        self.tabs.setTabText(self.tabs.indexOf(self.second_tab), _translate("MainWindow", "Train a new model"))
+        # self.tabs.setTabText(self.tabs.indexOf(self.first_tab), _translate("MainWindow", "Load a trained model"))
+        # self.tabs.setTabText(self.tabs.indexOf(self.second_tab), _translate("MainWindow", "Train a new model"))
 
     def openWindow(self, window: QMainWindow):
         self.w = window
