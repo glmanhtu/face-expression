0a34164c85d86f9ece13acb92c1764fe7faa01fd
Manh Tu <glmanhtu@gmail.com>
2019-07-08 20:34:30 +0100

    Optimise model

--- common/type/deep_model.py
+++ common/type/deep_model.py
@@ -18,21 +18,21 @@ def generate_model(n_classes, img_width, img_height):
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.35))
+    model.add(Dropout(0.2))
 
     # 2nd Convolution layer
     model.add(Conv2D(128, (3, 3), padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.35))
+    model.add(Dropout(0.25))
 
     # 3rd Convolution layer
     model.add(Conv2D(256, (3, 3), padding='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.35))
+    model.add(Dropout(0.3))
 
     # 4th Convolution layer
     model.add(Conv2D(512, (3, 3), padding='same'))
@@ -48,13 +48,13 @@ def generate_model(n_classes, img_width, img_height):
     model.add(Dense(256))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(Dropout(0.35))
+    model.add(Dropout(0.4))
 
     # 2nd Fully connected layer
     model.add(Dense(512))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(Dropout(0.35))
+    model.add(Dropout(0.45))
 
     # Output Fully connected layer
     model.add(Dense(n_classes))
